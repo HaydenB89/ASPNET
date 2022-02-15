fc041851159e31563d4f0d14698e9316f52d70f9
hayden brown <haydenbrowncinci@gmail.com>
2022-02-15 11:14:12 -0500

    started RequestController

---
+++ PRSLibraryProject/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using PRSLibraryProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibraryProject.Controllers {
+    public class ProductsController {
+        private readonly PRSDbContext _context;
+
+        public ProductsController(PRSDbContext context) {
+            this._context = context;
+        }
+
+        public IEnumerable<Product> GetAll() {
+            return _context.Products.ToList();
+        }
+
+        public Product GetByPk(int id) {
+            return _context.Products.Find(id);
+        }
+    }
+}
---
+++ PRSLibraryProject/Controllers/VendorController.cs
@@ -0,0 +1,44 @@
+﻿using PRSLibraryProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibraryProject.Controllers
+{
+    public class VendorsController {
+
+        private readonly PRSDbContext _context;
+
+        public VendorsController(PRSDbContext context) {
+            this._context = context;
+        }
+
+        public IEnumerable<Vendor> GetAll() {
+            return _context.Vendors.ToList();
+        }
+
+        public Vendor GetByPK(int id) {
+            return _context.Vendors.Find(id);
+        }
+
+        public Vendor Create(Vendor vendor) {
+            _context.Vendors.Add(vendor);
+            _context.SaveChanges();
+            return vendor;
+        }
+
+        public void Change(Vendor vendor) {
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id) {
+            var vendor = _context.Vendors.Find(id);
+            if (vendor != null) {
+                _context.Vendors.Remove(vendor);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
