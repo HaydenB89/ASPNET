0043fb36463a33db2d09d9fd035af51644ad50ab
hayden brown <haydenbrowncinci@gmail.com>
2022-02-15 14:33:07 -0500

    safety

---
+++ PRSLibraryProject/Controllers/RequestsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRSLibraryProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibraryProject.Controllers {
+    public class RequestsController {
+        private readonly PRSDbContext _context;
+
+        public RequestsController(PRSDbContext context) {
+            this._context = context;
+        }
+
+        public IEnumerable<Request> GetRequestsInReview(int userId) {
+            var requests = _context.Requests
+                                        Where(x => x.Status == "REVIEW"
+                                                && xUserId != userId)
+                                        ToList();
+            return requests;
+        }
+
+        public void SetApproved(Request request) {
+            request.Status = "APPROVED";
+            Change(request);
+        }
+
+        public void SetRejected(Request request) {
+            request.Status = "REJECTED";
+            Change(request);
+        }
+
+        private void Change(Request request) {
+            throw new NotImplementedException();
+        }
+
+        public void SetReview(Request request) {
+            if (request.Total <= 50) {
+                request.Status = "APPROVED";
+            }
+            else {
+                request.Status = "REVIEW";
+            }
+            Change(request);
+        }
+
+        public IEnumerable<Request> GetAll() {
+            return _context.Requests.Include(x => x.User).ToList();
+        }
+
+        public Request GetByPk(int id) {
+            return _context.Requests.Find(id);
+        }
+
+        public Request Create(Request request) {
+            if (request == null) {
+                throw new ArgumentNullException("request");
+            }
+            if (request.Id != 0) {
+                throw new ArgumentException("Request.Id must be zero!");
+            }
+            _context.Requests.Add(request);
+            _context.SaveChanges();
+            return request;
+        }
+
+        public void Delete(int id) {
+
+        }
+    }
+}
--- PRSTestConsoleProject/Program.cs
+++ PRSTestConsoleProject/Program.cs
@@ -12,43 +12,70 @@ namespace PRSTestConsoleProject {
 
             var context = new PRSDbContext();
 
-            var userController = new UsersController(context);
+            var userController = new RequestsController(context);
 
-            var newUser = new User() {
-                Id = 0, UserName = "xx", Password = "xx", FirstName ="User", LastName = "XX",
-                Phone = "211", Email = "xx@user.com", IsReviewer = false, IsAdmin = false
-            };
+            var reqs = reqCtrl.GetRequestsInReview(context);
 
-            //userController.Create(newUser);
-
-            var user3 = userController.GetByPK(3);
-
-            if(user3 is null) {
-                Console.WriteLine("User not found!");
-            }
-            else {
-                Console.WriteLine($"User3: {user3.FirstName} {user3.LastName}");
+            foreach (var req in reqs) {
+                Console.WriteLine($"{req.Description} {req.Status} {req.Total} {req.UserId}");
             }
 
-            user3.LastName = "User3";
-            userController.Change(user3);
+           // var req = reqCtrl.GetByPk(1);
 
-            var user33 = userController.GetByPK(33);
+            //reqCtrl.SetReview(req);
+            //reqCtrl.SetRejected(req);
+           // reqCtrl.SetApproved(req);
 
-            if (user33 is null) {
-                Console.WriteLine("User not found!");
-            }
-            else {
-                Console.WriteLine($"User3: {user33.FirstName} {user33.LastName}");
-            }
+           // req = reqCtrl.GetByPk(1);
 
-            userController.Remove(4);
+           // Console.WriteLine($"{req.Description} {req.Status} {req.Total}");
 
-            var users = userController.GetAll();       //we created the GetAll() method in the controller
 
-            foreach(var user in users) {
-                Console.WriteLine($"{user.Id} {user.FirstName} {user.LastName}");
-            }
+
+
+
+
+
+
+
+
+
+
+            //var newUser = new User() {
+            //    Id = 0, UserName = "xx", Password = "xx", FirstName ="User", LastName = "XX",
+            //    Phone = "211", Email = "xx@user.com", IsReviewer = false, IsAdmin = false
+            //};
+
+            ////userController.Create(newUser);
+
+            //var user3 = userController.GetByPK(3);
+
+            //if(user3 is null) {
+            //    Console.WriteLine("User not found!");
+            //}
+            //else {
+            //    Console.WriteLine($"User3: {user3.FirstName} {user3.LastName}");
+            //}
+
+            //user3.LastName = "User3";
+            //userController.Change(user3);
+
+            //var user33 = userController.GetByPK(33);
+
+            //if (user33 is null) {
+            //    Console.WriteLine("User not found!");
+            //}
+            //else {
+            //    Console.WriteLine($"User3: {user33.FirstName} {user33.LastName}");
+            //}
+
+            //userController.Remove(4);
+
+            //var users = userController.GetAll();       //we created the GetAll() method in the controller
+
+            //foreach(var user in users) {
+            //    Console.WriteLine($"{user.Id} {user.FirstName} {user.LastName}");
+            //}
         }
     }
 }
